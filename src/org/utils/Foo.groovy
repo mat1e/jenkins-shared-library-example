5c2a8d94b5919cb2890eb096e53a5a1d1e0ff4ff
mdelrocq <mathieu.delrocq@cgi.com>
2019-01-23 11:07:25 +0100

    test appel de method groovy

---
+++ src/org/utils/Foo.groovy
@@ -0,0 +1,7 @@
+package org.utils
+
+class Foo {
+	static String firstMethod() {
+		return "In first method"
+	}
+}
--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,7 @@
 #!/usr/bin/env groovy
+import org.utils.Foo
 
 def call(String name = 'human') {
 	echo "Hello, ${name}."
+	Foo.firstMethod()
 }
